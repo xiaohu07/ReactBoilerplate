84a3c3f8fa7ab3ee84f1229dac5afb0019953c47
mhu <marvin.hu@autotrader.com>
2016-12-14 21:11:05 -0500

    init commit

---
+++ app/app.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var { Route, Router, IndexRoute, hashHistory } = require('react-router');
+
+
+// Load foundation
+$(document).foundation();
+
+// App css
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+  <p>Boilerplate 3</p>,
+  document.getElementById('app')
+);
+
+
